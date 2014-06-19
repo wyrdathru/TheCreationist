916a13bc5e5fd81ccaacf7098319c922ee3fde44
Wyrdathru <wyrdathru@gmail.com>
2014-06-19 20:31:47 +0100

    Added template RuleManager and IColorRule interface for automation feature to come.

---
+++ Managers/RuleManager.cs
@@ -0,0 +1,73 @@
+﻿using GalaSoft.MvvmLight.Command;
+using ProjectVoid.TheCreationist.ViewModel;
+using System;
+
+namespace ProjectVoid.TheCreationist.Managers
+{
+    public class RuleManager : IDisposable
+    {
+        public RuleManager(MainViewModel mainViewModel)
+        {
+            MainViewModel = mainViewModel;
+
+            CreateRuleCommand = new RelayCommand(
+                () => CreateRule(),
+                () => CanCreateRule());
+
+            DeleteRuleCommand = new RelayCommand(
+                () => DeleteRule(),
+                () => CanDeleteRule());
+
+            ProcessRuleCommand = new RelayCommand<ProjectViewModel>(
+                (p) => ProcessRule(p),
+                (p) => CanProcessRule(p));
+        }
+
+        public MainViewModel MainViewModel { get; private set; }
+
+        public RelayCommand CreateRuleCommand { get; set; }
+
+        public RelayCommand DeleteRuleCommand { get; set; }
+
+        public RelayCommand<ProjectViewModel> ProcessRuleCommand { get; set; }
+
+        private void CreateRule()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CanCreateRule()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DeleteRule()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CanDeleteRule()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ProcessRule(ProjectViewModel projectViewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CanProcessRule(ProjectViewModel projectViewModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            Logger.Log.Debug("Disposing");
+
+            MainViewModel = null;
+
+            Logger.Log.Debug("Disposed");
+        }
+    }
+}
