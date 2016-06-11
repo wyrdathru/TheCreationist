01606a1e262acf5c105db5d7f7bf130d1a53dcfa
wyrdathru <wyrdathru@gmail.com>
2016-06-11 04:16:17 +0100

    Updated NuGet dependencies

--- ViewModel/ViewModelLocator.cs
+++ ViewModel/ViewModelLocator.cs
@@ -12,6 +12,7 @@
   See http://www.galasoft.ch/mvvm
 */
 
+using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
 using Microsoft.Practices.ServiceLocation;
 
@@ -30,18 +31,28 @@ namespace TheCreationist.App.ViewModel
         {
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
 
+            ////if (ViewModelBase.IsInDesignModeStatic)
+            ////{
+            ////    // Create design time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DesignDataService>();
+            ////}
+            ////else
+            ////{
+            ////    // Create run time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DataService>();
+            ////}
+
             SimpleIoc.Default.Register<MainViewModel>();
-            SimpleIoc.Default.Register<ProjectViewModel>();
         }
 
-        public MainViewModel MainViewModel
+        public MainViewModel Main
         {
             get
             {
                 return ServiceLocator.Current.GetInstance<MainViewModel>();
             }
         }
-
+        
         public static void Cleanup()
         {
             // TODO Clear the ViewModels
